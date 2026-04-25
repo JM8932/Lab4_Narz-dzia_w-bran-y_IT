b0bb2e8c243ac533d47c235536424a32b4b6fe49
JM8932 <jakub.merkel@vip.onet.pl>
2026-04-25 18:13:28 +0200

    Dodanie flagi --logs z obsluga argumentu

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 if [ "1$" == "--date" ]; then
 	date
+elif [ "1$" == "--logs" ]; then
+	count=${2:-100}
+	for i in $(seq 1 $count); do
+		echo "Plik: log${i}.txt | Skrypt: $0 | Data: $(date)" > log${i}.txt
+	done
 fi
+
