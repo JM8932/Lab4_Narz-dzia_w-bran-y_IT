f8f8377233b35e23be496a803ad2a319b83f4d0a
JM8932 <jakub.merkel@vip.onet.pl>
2026-04-25 18:18:59 +0200

    Dodanie flagi --help

--- skrypt.sh
+++ skrypt.sh
@@ -6,5 +6,10 @@ elif [ "1$" == "--logs" ]; then
 	for i in $(seq 1 $count); do
 		echo "Plik: log${i}.txt | Skrypt: $0 | Data: $(date)" > log${i}.txt
 	done
+elif [ "$1" == "--help" ]; then
+	echo "Instrukcja obslugi skryptu:"
+	echo "--date	: Wyswietla date i godzine"
+	echo "--logs [n]	: Tworzy n plikow log ( domyslne tworzy 100)"
+	echo "--help		: Wyswietla te liste opcji "
 fi
 
