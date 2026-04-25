9ad72b88624eebc8d46a97b549ddbdaf0216ed69
JM8932 <jakub.merkel@vip.onet.pl>
2026-04-25 19:00:26 +0200

    Czesc 2: Dodanie krotkich flag -d -l oraz -h

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-if [ "1$" == "--date" ]; then
-	date
-elif [ "1$" == "--logs" ]; then
-	count=${2:-100}
-	for i in $(seq 1 $count); do
-		echo "Plik: log${i}.txt | Skrypt: $0 | Data: $(date)" > log${i}.txt
-	done
-elif [ "$1" == "--help" ]; then
-	echo "Instrukcja obslugi skryptu:"
-	echo "--date	: Wyswietla date i godzine"
-	echo "--logs [n]	: Tworzy n plikow log ( domyslne tworzy 100)"
-	echo "--help		: Wyswietla te liste opcji "
-fi
+
+case "$1" in
+	--date | -d)
+	  date 
+	  ;;
+	--logs | -l)
+	  count=${2:-100}
+	  for i in $(seq 1 $count); do
+	   echo "Plik: log${i}.txt | Skrypt: $0 | Data: $(date)" > log${i}.txt
+	  done
+	  ;;
+	--help | -h)
+	   echo "Opcje: --date (-d), --logs  (-l), --help (-h), --init, --error (-e)"
+	  ;;
+esac
 
